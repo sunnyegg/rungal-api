1e271172ade508d60687f6e0e2f6897d19829568
sunnyegg <53990968+sunnyegg@users.noreply.github.com>
2019-09-24 11:49:51 +0700

    Update categories.js

--- src/models/categories.js
+++ src/models/categories.js
@@ -17,37 +17,37 @@ module.exports = {
     addCategory: (data) => {
         return new Promise((resolve, reject) => {
             conn.query('INSERT INTO category SET ?', data,
-                (err, result) => {
-                    if (!err) {
-                        resolve(result)
-                    } else {
-                        reject(new Error(err))
-                    }
-                })
+            (err, result) => {
+                if (!err) {
+                    resolve(result)
+                } else {
+                    reject(new Error(err))
+                }
+            })
         })
     },
     editCategory: (data, id) => {
         return new Promise((resolve, reject) => {
             conn.query('UPDATE category SET ? WHERE ?', [data, id],
-                (err, result) => {
-                    if (!err) {
-                        resolve(result)
-                    } else {
-                        reject(new Error(err))
-                    }
-                })
+            (err, result) => {
+                if (!err) {
+                    resolve(result)
+                } else {
+                    reject(new Error(err))
+                }
+            })
         })
     },
     deleteCategory: (id) => {
         return new Promise((resolve, reject) => {
             conn.query('DELETE FROM category WHERE ?', [id],
-                (err, result) => {
-                    if (!err) {
-                        resolve(result)
-                    } else {
-                        reject(new Error(err))
-                    }
-                })
+            (err, result) => {
+                if (!err) {
+                    resolve(result)
+                } else {
+                    reject(new Error(err))
+                }
+            })
         })
     }
 }
