4456999a0907cbeaa59eebec0fedf29eaff7f358
sunnyegg <53990968+sunnyegg@users.noreply.github.com>
2019-09-24 11:49:37 +0700

    Update products.js
    add features: add/remove quantity

--- src/models/products.js
+++ src/models/products.js
@@ -14,40 +14,76 @@ module.exports = {
             })
         })
     },
+    getProductbyID: (id) => {
+        return new Promise((resolve, reject) => {
+            conn.query('SELECT * FROM product WHERE ?', id,
+            (err, result) => {
+                if(!err) {
+                    resolve(result)
+                } else {
+                    reject(new Error(err))
+                }
+            })
+        })
+    },
     addProduct: (data) => {
         return new Promise((resolve, reject) => {
             conn.query('INSERT INTO product SET ?', data,
-                (err, result) => {
-                    if (!err) {
-                        resolve(result)
-                    } else {
-                        reject(new Error(err))
-                    }
-                })
+            (err, result) => {
+                if (!err) {
+                    resolve(result)
+                } else {
+                    reject(new Error(err))
+                }
+            })
+        })
+    },
+    addQuantityProduct: (qty, id) => {
+        return new Promise((resolve, reject) => {
+            conn.query('UPDATE product SET quantity = quantity + ? WHERE ?', [qty, id],
+            (err, result) => {
+                if(!err) {
+                    resolve(result)
+                } else {
+                    reject(new Error(err))
+                }
+            })
+        })
+    },
+    removeQuantityProduct: (qty, id) => {
+        return new Promise((resolve, reject) => {
+            conn.query('UPDATE product SET quantity = quantity - ? WHERE ?', [qty, id],
+            (err, result) => {
+                if(!err) {
+                    resolve(result)
+                } else {
+                    reject(new Error(err))
+                }
+            })
         })
     },
     editProduct: (data, id) => {
         return new Promise((resolve, reject) => {
             conn.query('UPDATE product SET ? WHERE ?', [data, id],
-                (err, result) => {
-                    if (!err) {
-                        resolve(result)
-                    } else {
-                        reject(new Error(err))
-                    }
-                })
+            (err, result) => {
+                if (!err) {
+                    resolve(result)
+                } else {
+                    reject(new Error(err))
+                }
+            })
         })
     },
     deleteProduct: (id) => {
         return new Promise((resolve, reject) => {
             conn.query('DELETE FROM product WHERE ?', [id],
-                (err, result) => {
-                    if (!err) {
-                        resolve(result)
-                    } else {
-                        reject(new Error(err))
-                    }
-                })
+            (err, result) => {
+                if (!err) {
+                    resolve(result)
+                } else {
+                    reject(new Error(err))
+                }
+            })
         })
     }
 }
