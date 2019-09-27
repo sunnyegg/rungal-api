259a3d9ed3165c5f1c329beb28b8a486357ba5bb
sunnyegg <53990968+sunnyegg@users.noreply.github.com>
2019-09-27 08:32:56 +0700

    Update products.js
    add feature upload file (image jpg/png only)

--- src/routes/products.js
+++ src/routes/products.js
@@ -7,12 +7,10 @@ const productsController = require('../controllers/products')
 
 Route
     .get('/products', productsController.getProducts)
-    .get('/products/:id', productsController.getProductbyID)
     .post('/products', productsController.addProduct)
     .patch('/products/order/:id', productsController.addQuantityProduct)
     .patch('/products/reduce/:id', productsController.reduceQuantityProduct)
     .put('/products/:id', productsController.editProduct)
     .delete('/products/:id', productsController.deleteProduct)
-
 
 module.exports = Route
