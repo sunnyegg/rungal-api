57ce3be0cbca1ebb4e5509c160c9a7e7324f893e
sunnyegg <53990968+sunnyegg@users.noreply.github.com>
2019-09-27 17:33:34 +0700

    Create login.js
    add dotenv & JWT login/register

---
+++ src/routes/login.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const Route = express.Router()
+
+const usersController = require('../controllers/users')
+
+Route
+    .post('/register', usersController.registerUser)
+    .post('/login', usersController.loginUser)
+
+module.exports = Route
