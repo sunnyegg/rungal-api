987d7594b577b7acc5249bf5680c352120a18e22
sunnyegg <53990968+sunnyegg@users.noreply.github.com>
2019-09-27 17:33:31 +0700

    Update categories.js
    add dotenv & JWT login/register

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -1,15 +1,16 @@
 // import all the required modules
 const express = require('express')
 const Route = express.Router()
+const methods = require('../methods')
 
 // import controller
 const categoriesController = require('../controllers/categories')
 
 Route
     .get('/categories', categoriesController.getCategories)
-    .post('/categories', categoriesController.addCategory)
-    .put('/categories/:id', categoriesController.editCategory)
-    .delete('/categories/:id', categoriesController.deleteCategory)
+    .post('/categories', methods.ensureToken, categoriesController.addCategory)
+    .put('/categories/:id', methods.ensureToken, categoriesController.editCategory)
+    .delete('/categories/:id', methods.ensureToken, categoriesController.deleteCategory)
 
 
 module.exports = Route
