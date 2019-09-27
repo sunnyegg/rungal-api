7e1ebdbc58bc06df1a126d33dc0b417a2b9fa26b
sunnyegg <53990968+sunnyegg@users.noreply.github.com>
2019-09-27 17:33:21 +0700

    Update index.js
    add dotenv & JWT login/register

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ const Route = express.Router()
 // defines all routes
 const products = require('./routes/products')
 const categories = require('./routes/categories')
+const login = require('./routes/login')
 
 Route
     .use('/api/v1', products)
     .use('/api/v1', categories)
+    .use('/api/v1', login)
 
 module.exports = Route
