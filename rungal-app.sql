efb3c833f5ac3876e81f9738134c734ac1ec5647
sunnyegg <adilatresna21@gmail.com>
2019-10-12 18:40:24 +0700

    update db

--- rungal-app.sql
+++ rungal-app.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Oct 08, 2019 at 09:27 AM
+-- Generation Time: Oct 12, 2019 at 06:34 PM
 -- Server version: 5.7.27-0ubuntu0.18.04.1
 -- PHP Version: 7.2.19-0ubuntu0.18.04.2
 
@@ -44,6 +44,37 @@ INSERT INTO `category` (`id`, `name`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `history`
+--
+
+CREATE TABLE `history` (
+  `id` int(20) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `price` int(10) NOT NULL,
+  `quantity` int(10) NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `invoice` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `history`
+--
+
+INSERT INTO `history` (`id`, `name`, `price`, `quantity`, `date_created`, `invoice`) VALUES
+(4, 'Egg', 50000, 10, '2019-10-03 16:28:59', 7901),
+(5, 'Noodle', 30000, 10, '2019-10-04 16:29:49', 3452),
+(6, 'Aqua', 40000, 10, '2019-09-18 16:48:15', 9405),
+(7, 'Cendol', 60000, 10, '2019-10-10 21:50:58', 204),
+(8, 'Coffee', 60000, 10, '2019-10-09 21:51:07', 6417),
+(9, 'Iced Coffee', 80000, 10, '2019-09-20 21:51:19', 4991),
+(10, 'Iced Tea', 50000, 10, '2019-10-11 21:51:29', 6632),
+(11, 'Meatball', 70000, 10, '2019-10-11 21:51:39', 4586),
+(12, 'Meatball', 70000, 10, '2019-10-12 01:10:01', 9751),
+(13, 'Cendol', 60000, 10, '2019-10-12 10:29:28', 876);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `product`
 --
 
@@ -77,7 +108,7 @@ INSERT INTO `product` (`id`, `name`, `description`, `image`, `category`, `price`
 (11, 'Iced Water', '', 'rungal-img-1570028189565water.jpg', 2, 3000, 100, '2019-09-24 00:00:00', '2019-10-02 21:56:30'),
 (12, 'Coffee', '', 'rungal-img-1570028385551coffee.jpg', 2, 6000, 100, '2019-09-24 00:00:00', '2019-10-02 21:59:46'),
 (13, 'Iced Coffee', '', 'rungal-img-1570028465496iced coffee.jpg', 2, 8000, 100, '2019-09-24 00:00:00', '2019-10-02 22:01:05'),
-(14, 'Aqua', '', 'rungal-img-1570028724650aqua.jpg', 2, 4000, 100, '2019-09-25 00:00:00', '2019-10-02 22:05:25'),
+(14, 'Aqua', '', 'rungal-img-1570629142633aqua.jpg', 1, 4000, 100, '2019-09-25 00:00:00', '2019-10-09 20:52:23'),
 (15, 'Nasi Pecel', '', 'rungal-img-1570029089712nasi pecel.jpg', 1, 6000, 50, '2019-09-26 00:00:00', '2019-10-02 22:11:30'),
 (16, 'Nasi Kuning', '', 'rungal-img-1570461416579nasi kuning.jpg', 1, 7000, 20, '2019-10-07 22:16:57', '2019-10-07 22:16:57');
 
@@ -99,7 +130,8 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`id`, `user`, `password`) VALUES
 (1, 'test123', '$2a$10$xf6wJjNXkJOjMRwxMY8HauIXoPquE.GKCflVFGBSsXZS7PZadNVlS'),
-(2, 'yeay', '$2a$10$1JIGyvei8WK9f6gsrpmHpOUf3v1V1cGe4N1d.cfFZFmDkvHkar3Du');
+(2, 'yeay', '$2a$10$1JIGyvei8WK9f6gsrpmHpOUf3v1V1cGe4N1d.cfFZFmDkvHkar3Du'),
+(3, 'Adila', '$2a$10$FuRSyliry1h1WxUKNAljnuLRpdSMEe/1A7ApbeZ.p3zYynXncsLlC');
 
 --
 -- Indexes for dumped tables
@@ -111,6 +143,12 @@ INSERT INTO `user` (`id`, `user`, `password`) VALUES
 ALTER TABLE `category`
   ADD PRIMARY KEY (`id`),
   ADD KEY `name` (`name`);
+
+--
+-- Indexes for table `history`
+--
+ALTER TABLE `history`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `product`
@@ -136,6 +174,12 @@ ALTER TABLE `category`
   MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT for table `history`
+--
+ALTER TABLE `history`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
@@ -145,7 +189,7 @@ ALTER TABLE `product`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
