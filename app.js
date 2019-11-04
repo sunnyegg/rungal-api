d15cd6afc413e75c224cc8fb861e09d48edb7a69
Adila <adilatresna21@gmail.com>
2019-11-04 22:17:36 +0700

    fix lagi

--- app.js
+++ app.js
@@ -1,40 +1,37 @@
 // import all the required modules
-const express = require('express')
-const bodyParser = require('body-parser')
-const logger = require('morgan')
-const cors = require('cors')
-const fileUpload = require('express-fileupload')
-require('dotenv/config')
+const express = require("express");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
+require("dotenv/config");
 
-const app = express() // use the express
+const app = express(); // use the express
 
 // enable fileupload
-app.use(fileUpload())
-app.use(express.static('./src/assets/img/'))
+app.use(fileUpload());
+app.use(express.static("./src/assets/img/"));
 
 // parsing input
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // cors
-app.use(cors())
+app.use(cors());
 
 // logging activities
-app.use(logger('dev'))
-
-// port
-const port = process.env.SERVER_PORT || 3000
+app.use(logger("dev"));
 
 // app listening to
-app.listen(port, () => {
-  console.log(`Server listening on: ${port}`)
-})
+app.listen(process.env.SERVER_PORT || 3000, () => {
+  console.log(`Server listening on: ${port}`);
+});
 
 // import all routes
-const routerNav = require('./src/index')
-app.use('/', routerNav)
+const routerNav = require("./src/index");
+app.use("/", routerNav);
 
 // determines all other routes
-app.get('*', (req, res) => {
-  res.send('The page you are looking for is not found.')
-})
+app.get("*", (req, res) => {
+  res.send("The page you are looking for is not found.");
+});
